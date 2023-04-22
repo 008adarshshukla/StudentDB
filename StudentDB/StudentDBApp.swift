e4ceff46d210b08ee3ed078525d39c95d31c4789
008adarshshukla <shuklaadarsh1932@gmail.com>
2023-04-22 10:01:20 +0530

    Initial Commit

---
+++ StudentDB/StudentDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentDBApp.swift
+//  StudentDB
+//
+//  Created by Adarsh Shukla on 22/04/23.
+//
+
+import SwiftUI
+
+@main
+struct StudentDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
